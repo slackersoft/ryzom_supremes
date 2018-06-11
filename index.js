a12e3dbab4880ff9ebbd3bbd2f20f4e59b663e2a
Gregg Van Hove <gregg@slackersoft.net>
2018-06-10 21:10:12 -0700

    Predict further out and add some styling

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const ryzomTime = require('./lib/ryzomTime');
 const humidity = require('./lib/humidity');
+const WeatherReport = require('./lib/weatherReport');
 
 let app = express();
 app.use(morgan('combined'));
+app.set('view engine', 'ejs');
+
+app.use('/css', express.static('css'));
 
 app.get('/', async (req, res) => {
   let currentTime = await ryzomTime.fetchCurrentTime();
   let times = [currentTime];
-  while (times.length < 10) {
+  while (times.length < 24) {
     times.push(times[times.length - 1].next());
   }
-  times.forEach(time => {
-    time.humidity = Math.floor(humidity(time.cycleNumber) * 100) + '%';
-  });
-  res.json(times);
+  let weather = times.map(time => new WeatherReport(time, Math.floor(humidity(time.cycleNumber) * 100)));
+  res.render('index', { weather });
 });
 
 app.listen(process.env.PORT || 3000);
