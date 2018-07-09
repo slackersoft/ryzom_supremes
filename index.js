5cf4fd3e220f5e0dee2fa7a2e8b70a02c416ca84
Gregg Van Hove <gregg@slackersoft.net>
2018-07-08 21:17:45 -0700

    show materials for current weather

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const ryzomTime = require('./lib/ryzomTime');
 const humidity = require('./lib/humidity');
+const materials = require('./lib/materials');
 const WeatherReport = require('./lib/weatherReport');
 
 let app = express();
@@ -17,7 +18,11 @@ app.get('/', async (req, res) => {
     times.push(times[times.length - 1].next());
   }
   let weather = times.map(time => new WeatherReport(time, Math.floor(humidity(time.cycleNumber) * 100)));
-  res.render('index', { weather });
+  let supremes = [];
+  if (req.query.zone) {
+    supremes = materials.supremes(currentTime.season, weather[0].humidity, req.query.zone);
+  }
+  res.render('index', { weather, supremes, zone: req.query.zone });
 });
 
 app.listen(process.env.PORT || 3000);
