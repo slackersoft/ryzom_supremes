d5c225ac5ebd9e4397b24e3d7397f4e09f1546b7
Gregg Van Hove <gregg@slackersoft.net>
2018-08-12 21:44:14 -0700

    So many things
    - Inline some styles so colors render in game
    - Add an icon
    - Calculate offset from when time API cached the time
    - Deployment stuff

--- index.js
+++ index.js
@@ -19,6 +19,46 @@ app.use(morgan('combined'));
 app.set('view engine', 'ejs');
 
 app.use('/css', express.static('css'));
+app.use('/images', express.static('images'));
+
+if (process.env['NODE_ENV'] === 'production') {
+  const cfenv = require('cfenv');
+  const appEnv = cfenv.getAppEnv();
+  const secrets = appEnv.getServiceCreds('secrets');
+  const cookieSession = require('cookie-session');
+  const auth = require('./lib/auth');
+  app.use(cookieSession({
+    name: 'session',
+    secret: secrets.cookie
+  }));
+  app.use(auth(secrets.appKey));
+}
+
+function sups(allWeather, zone, startIndex) {
+  return {
+    mats: materials.supremes(allWeather[startIndex].ryzomTime.season, allWeather[startIndex].humidity, zone),
+    weather: allWeather[startIndex],
+  };
+}
+
+function upcomingWeatherBands(weather) {
+  const bands = [];
+  weather.forEach(w => {
+    if (bands.length === 0) {
+      bands.push({
+        weather: w
+      });
+    } else if (!bands[bands.length - 1].endTime && w.weatherClass !== bands[bands.length - 1].weather.weatherClass) {
+      bands[bands.length - 1].endTime = w.ryzomTime;
+    }
+    if (bands[bands.length - 1].endTime && bands.map(b => b.weather.weatherClass).every(wc => wc !== w.weatherClass)) {
+      bands.push({
+        weather: w
+      });
+    }
+  });
+  return bands;
+}
 
 app.get('/', async (req, res) => {
   let currentTime = await ryzomTime.fetchCurrentTime();
@@ -29,10 +69,12 @@ app.get('/', async (req, res) => {
   let weather = times.map(time => new WeatherReport(time, Math.floor(humidity(time.cycleNumber) * 100)));
   let supremes = {};
   if (req.query.zone) {
-    supremes.current = {
-      mats: materials.supremes(currentTime.season, weather[0].humidity, req.query.zone),
-      weather: weather[0],
-    };
+    console.dir(upcomingWeatherBands(weather));
+    supremes.current = sups(weather, req.query.zone, 0);
+//    {
+//      mats: materials.supremes(weather[0].ryzomTime.season, weather[0].humidity, req.query.zone),
+//      weather: weather[0],
+//    };
     const nextChange = weather.find(w => w.weatherClass !== weather[0].weatherClass);
     supremes.next = {
       mats: materials.supremes(nextChange.ryzomTime.season, nextChange.humidity, req.query.zone),
@@ -49,7 +91,15 @@ app.get('/', async (req, res) => {
       weather: lastOne,
     };
   }
+
   res.render('index', { weather, supremes, zones, seasons, zone: req.query.zone });
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status_code || 500).send(err.message);
 });
 
 app.listen(process.env.PORT || 3000);
